b353d83aad88542e940aa7f32712fbdb4b586b39
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-11-13 17:45:22 +0300

    contact us added

--- Connect/src/components/LandingPage.jsx
+++ Connect/src/components/LandingPage.jsx
@@ -1,22 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ServiceContainer from "./ServicesContainer";
 import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
-    <div class="containerstyle">
+    <div className="containerstyle">
       <h1>Welcome to Connect</h1>
       <p>
         New in an area? Worry not. We make it easy for you to connect with all
         the service providers in your neighborhood
       </p>
-      <button
-        type="button"
-        class="btn btn-success"
-        onClick={() => {
-          ServiceContainer;
-        }}
-      >
+      <button type="button" className="btn btn-success">
         Get Our Services
       </button>
     </div>
--- Connect/src/components/Services.jsx
+++ Connect/src/components/Services.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Services() {
   return (
     <>
-      <div className="card" style="width: 18rem;">
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <p className="card-text">
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
