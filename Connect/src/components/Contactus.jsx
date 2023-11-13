c223aec2e004dca6a4f019e4d93cf6012328b31f
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-11-13 16:40:58 +0300

    contact us form added

--- Connect/src/components/Contactus.jsx
+++ Connect/src/components/Contactus.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 function Contactus() {
-  return <p>You can contact us here</p>;
+  return (
+    <div className="container mt-5">
+      <h2 className="text-center mb-4">Contact Us</h2>
+
+      <form>
+        <div className="form-group">
+          <label htmlFor="name">Your Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            placeholder="Enter your name"
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Your Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="message">Your Message</label>
+          <textarea
+            className="form-control"
+            id="message"
+            rows="4"
+            placeholder="Type your message here"
+            required
+          ></textarea>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Contactus;
